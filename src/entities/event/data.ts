8d922dabdcf886086aa25c10ab8132cec70d556c
Lev <vinnipyx38@gmail.com>
2025-04-22 09:25:05 +0500

    Добавили мокнутые данные

--- src/entities/event/data.ts
+++ src/entities/event/data.ts
@@ -8,7 +8,7 @@ export const defaultState: IEvent[] = [
     shortDescription:
       "это идеальный способ отдохнуть, зарядиться азартом и провести время в отличной компании",
     fullDescription: "",
-    link: "#",
+    link: "https://whitelanes.ru/",
     textPosition: "bottom",
     type: EventCategories.Happy,
   },
@@ -19,7 +19,7 @@ export const defaultState: IEvent[] = [
     shortDescription:
       "первая в России и единственная в Вашем городе студия для игры в Мафию с падающими стульями",
     fullDescription: "",
-    link: "#",
+    link: "https://mafiavip.ru/rooms/city/perm",
     textPosition: "bottom",
     type: EventCategories.Happy,
   },
@@ -30,7 +30,7 @@ export const defaultState: IEvent[] = [
     shortDescription:
       "это открытый картинг-центр, где вы можете покататься на бензиновых и электрических картах, а также на картах для детей с ограничением скорости.",
     fullDescription: "",
-    link: "#",
+    link: "https://kart-on.ru/",
     textPosition: "top",
     type: EventCategories.Happy,
   },
@@ -41,52 +41,142 @@ export const defaultState: IEvent[] = [
     shortDescription:
       "это захватывающая игра, где вы, будучи одним из героев, проходите запланированный сюжет.",
     fullDescription: "",
-    link: "#",
+    link: "https://questhunter.info/perm/",
     textPosition: "top",
     type: EventCategories.Happy,
   },
   {
-    id: 2,
-    title: "Мастер-класс по кулинарии",
-    image: "https://placehold.co/600x400?text=Мастер-класс",
-    shortDescription: "Узнайте секреты шеф-повара",
-    fullDescription:
-      "На мастер-классе вы научитесь готовить изысканные блюда под руководством профессионала.",
-    link: "#",
-    textPosition: "bottom",
+    id: 6,
+    title: "Лепи Нежно",
+    shortDescription:
+      "это про нежность и заботу во всем!\n" +
+      "Нежно лепим руками, нежно лепим за гончарным кругом, нежно и бережно сопровождаем ваши изделия",
     type: EventCategories.MasterClass,
+    textPosition: "top",
+    fullDescription: "",
+    image: "public/mk1.png",
+    link: "https://lepinejno.ru/",
   },
   {
-    id: 3,
-    title: "Обзор лучших кафе города",
-    image: "https://placehold.co/600x400?text=Где+покушать",
-    shortDescription: "Где вкусно покушать?",
-    fullDescription:
-      "Подробное описание лучших заведений для гурманов, уютные кафе и рестораны.",
-    link: "#",
+    id: 6,
+    title: "Школа кондитеров\n" + "Felicia",
+    shortDescription:
+      "Научись виртуозно создавать десерты под чутким руководством шеф-кондитера!",
+    type: EventCategories.MasterClass,
     textPosition: "top",
+    fullDescription: "",
+    image: "public/mk2.png",
+    link: "https://felicia-school.ru/",
+  },
+  {
+    id: 6,
+    title: "Nolan Wine & Kitchen",
+    shortDescription:
+      "ресторан который предлагает своим гостям разнообразное меню, включающее в себя блюда из рыбы и морепродуктов, а также мясные блюда и десерты.",
     type: EventCategories.Eat,
-  },
-  {
-    id: 4,
-    title: "Летний отдых на природе",
-    image: "https://placehold.co/600x400?text=Где+отдохнуть",
-    shortDescription: "Идеальное место для отдыха",
-    fullDescription:
-      "Подробное описание мест для отдыха на природе, свежий воздух и активный отдых на свежем воздухе.",
-    link: "#",
-    textPosition: "bottom",
-    type: EventCategories.Relax,
-  },
-  {
-    id: 5,
-    title: "Культурный вечер в музее",
-    image: "https://placehold.co/600x400?text=Культурные+мероприятия",
-    shortDescription: "Ночь в музее",
-    fullDescription:
-      "Подробное описание культурного мероприятия с экскурсиями, лекциями и творческими мастер-классами.",
-    link: "#",
     textPosition: "top",
+    fullDescription: "",
+    image: "public/e1.png",
+    link: "https://nolan-wine-kitchen.clients.site/",
+  },
+  {
+    id: 6,
+    title: "Цех - кофейня",
+    shortDescription:
+      "уютное место где можно позавтракать, пообедать или поужинать, а также насладиться ароматным кофе и вкусными десертами.",
+    type: EventCategories.Eat,
+    textPosition: "top",
+    fullDescription: "",
+    image: "public/e2.png",
+    link: "https://yandex.ru/maps/org/tsekh/29138344521/?ll=56.247742%2C58.000074&z=16",
+  },
+  {
+    id: 6,
+    title: "Чайка ZaZa",
+    shortDescription:
+      "это стильное место с современным интерьером, выполненным из натуральных материалов. Просторные залы с большими окнами, откуда открывается вид на реку Каму, создают ощущение простора и свободы.",
+    type: EventCategories.Eat,
+    textPosition: "bottom",
+    fullDescription: "",
+    image: "public/e3.png",
+    link: "https://zazarest.ru/",
+  },
+  {
+    id: 6,
+    title: "Кафе Barberry Cabare",
+    shortDescription:
+      "это кафе, которое специализируется на клубнике в шоколаде. Интерьер кафе выполнен в стиле кабаре, с использованием красного бархата и роскошных люстр.",
+    type: EventCategories.Eat,
+    textPosition: "top",
+    fullDescription: "",
+    image: "public/e4.png",
+    link: "https://yandex.ru/maps/org/barberry_cabare/177456238308/?ll=56.236315%2C58.012319&z=16",
+  },
+  {
+    id: 6,
+    title: "Глэмпинг WELCOME TO FOREST",
+    shortDescription:
+      "это глэмпинг, расположенный в лесу недалеко от Демидково.",
+    type: EventCategories.Relax,
+    textPosition: "top",
+    fullDescription: "",
+    image: "public/r1.png",
+    link: "https://welcome2parma.ru/",
+  },
+  {
+    id: 6,
+    title: "Термальный комплекс - Акватория",
+    shortDescription:
+      "это термальный комплекс, расположенный в живописном месте недалеко от города. Он предлагает своим гостям широкий спектр услуг, включая открытый подогреванемый бассейн, баню, сауну, хаммам, массаж и кафе.",
+    type: EventCategories.Relax,
+    textPosition: "bottom",
+    fullDescription: "",
+    image: "public/r2.png",
+    link: "http://akvaterm59.ru/",
+  },
+  {
+    id: 6,
+    title: "Санаторий «Демидково»",
+    shortDescription:
+      "расположен в сосновом бору на берегу реки Камы. Гостям предлагается проживание в комфортабельных номерах, а также в уютных эко-домиках с просторными верандами и видом на лес.",
+    type: EventCategories.Relax,
+    textPosition: "top",
+    fullDescription: "",
+    image: "public/r3.png",
+    link: "https://demidkovo.ru/",
+  },
+  // Добавьте остальные события
+  {
+    id: 6,
+    title: "Permm",
+    shortDescription:
+      "это современное пространство для проведения различных арт-мероприятий, научных и образовательных событий. Здесь можно познакомиться с необычными и глубокими по смыслу произведениями современного искусства.",
     type: EventCategories.Cultural,
+    textPosition: "top",
+    fullDescription: "",
+    image: "public/c1.png",
+    link: "https://permm.ru/",
+  },
+  {
+    id: 6,
+    title: "Пермский академический Театр-Театр",
+    shortDescription:
+      "один из старейших театров Пермского края, основанный 14 марта 1927 года. В театре есть три сценические площадки: Большая сцена, «Сцена-Молот» и детская сцена «Театр-Тятрик».",
+    type: EventCategories.Cultural,
+    textPosition: "top",
+    fullDescription: "",
+    image: "public/c2.png",
+    link: "https://teatr-teatr.com/",
+  },
+  {
+    id: 6,
+    title: "Театр оперы и балета",
+    shortDescription:
+      "один из старейших музыкальных театров России. Он получил большую известность не только на родине, но и за пределами бывшего СССР и России.",
+    type: EventCategories.Cultural,
+    textPosition: "top",
+    fullDescription: "",
+    image: "public/c3.png",
+    link: "https://permopera.ru/",
   },
 ];
